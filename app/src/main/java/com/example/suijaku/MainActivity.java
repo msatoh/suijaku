fa0b439f3a7dd0a10b81cddc5db0c3d16119c228
msatoh <msatoh@hotmail.co.jp>
2020-05-25 22:08:14 +0900

    ボタン配置、トースト

--- app/src/main/java/com/example/suijaku/MainActivity.java
+++ app/src/main/java/com/example/suijaku/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.suijaku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void game_start(View view){
+        Toast myToast = Toast.makeText(
+                getApplicationContext(),
+                "ポップアップ！！",
+                Toast.LENGTH_SHORT
+        );
+        myToast.show();
     }
 }
