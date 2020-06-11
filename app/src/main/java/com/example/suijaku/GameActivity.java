cf03412fabeedd57a3ed0003caa2f2d73166f579
msatoh <msatoh@hotmail.co.jp>
2020-06-11 23:32:58 +0900

    カード表示クラス作成途中

--- app/src/main/java/com/example/suijaku/GameActivity.java
+++ app/src/main/java/com/example/suijaku/GameActivity.java
@@ -4,12 +4,30 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Card{
-    private enum mark{heart,spade,dia,club};
+import java.util.ArrayList;
+import java.util.Random;
+
+enum mark{heart,spade,dia,club}
+
+class Player{
+    private String name;
+    private ArrayList card_list;
+    public void set_name(String name_in){
+        name=name_in;
+    }
+    public void set_card(Card card_in){
+        card_list.add(card_in);
+    }
+    public ArrayList show_and_list(){
+        return card_list;
+    }
+}
+
+class Card{
     private mark mark_use;
     private String number;
     private int strength;
-    private int serial_number;
+//    private int serial_number;
     public class card_for_display{
         private mark mark_for_display;
         private String number_for_display;
@@ -17,7 +35,7 @@ public class Card{
     public int return_strength(){
         return strength;
     }
-    public card_for_display display_card(int in_serial_number) {
+    public card_for_display display_card() {
         card_for_display card_for_display_class = new card_for_display();
         card_for_display_class.mark_for_display = mark_use;
         card_for_display_class.number_for_display = number;
@@ -48,20 +66,68 @@ public class Card{
                 break;
         }
     }
-    public void give_serial_number(int serial_number_in){
-        serial_number=serial_number_in;
-    }
-}
-
-public void shuffle(){
 
 }
 
 public class GameActivity extends AppCompatActivity {
+
+    private boolean[] already_used=new boolean[52];
+
+    private void initialize_array(boolean[] already_used_in){
+        int counter;
+        for(counter=0;counter<52;counter++) {
+            already_used_in[counter]=false;
+        }
+    }
+
+    private Card generate_random_card(){
+        mark mark_in;
+        Card one_card=new Card();
+        Random mark_number_seed=new Random();
+        int mark_number=mark_number_seed.nextInt(4);
+        int strength_number=mark_number_seed.nextInt(13)+1;
+        while(already_used[mark_number * 4 + strength_number - 1]){
+            mark_number=mark_number_seed.nextInt(4);
+            strength_number=mark_number_seed.nextInt(13)+1;
+        }already_used[mark_number * 4 + strength_number - 1]=true;
+        switch(mark_number) {
+            case 0:
+                mark_in = mark.heart;
+                break;
+            case 1:
+                mark_in = mark.spade;
+                break;
+            case 2:
+                mark_in = mark.dia;
+                break;
+            case 3:
+                mark_in = mark.club;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + mark_number);
+        }
+        one_card.give_mark(mark_in);
+        one_card.give_strength(strength_number);
+        return one_card;
+    }
+
+    private void devide_card(Player man_in,Card card_in){
+        man_in.set_card(card_in);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_avtivity);
-
+        initialize_array(already_used);
+        Player man=new Player();
+        int counter;
+        for(counter=0;counter<7;counter++) {
+            devide_card(man, generate_random_card());
+        }
+        for(counter=0;counter<7;counter++){
+            man.show_and_list().get(counter);
+        }
     }
+
 }
