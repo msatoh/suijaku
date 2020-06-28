925afb0aa9aea2f333120c5c505917169c7fe1c4
msatoh <msatoh@hotmail.co.jp>
2020-06-28 12:01:01 +0900

    2枚出し対応途中

--- app/src/main/java/com/example/suijaku/Brain.java
+++ app/src/main/java/com/example/suijaku/Brain.java
@@ -9,14 +9,67 @@ public class Brain {
         //Random num_seed=new Random();
         ArrayList<Card> empty_card=new ArrayList<>();
         int candidate;
+        int first=1,second=mycard.size()-1,third=mycard.size()-1,fourth=mycard.size()-1;
         ArrayList<Card> candidate_card=new ArrayList<>();
-        for(candidate=0;candidate<mycard.size();candidate++){
-            candidate_card.clear();
-            candidate_card.add(mycard.get(candidate));
+        if(card_field.size()>1){
+            second=2;
+        }
+        if(card_field.size()>2){
+            third=3;
+        }
+        if((card_field.size()>3)){
+            fourth=4;
+        }
+        candidate_card.clear();
+        for(first=0;first<mycard.size();first++) {
+            candidate_card.add(mycard.get(first));
             if(checker.check_if_decideable(candidate_card,card_field)){
                 return candidate_card;
             }
+            for(second=second+first-1;second<mycard.size();second++){
+                if(candidate_card.indexOf(mycard.get(second))!=-1) {
+                    candidate_card.add(mycard.get(second));
+                }
+                if(checker.check_if_decideable(candidate_card,card_field)){
+                    return candidate_card;
+                }
+                for(third=third+second-2;third<mycard.size();third++){
+                    if(candidate_card.indexOf(mycard.get(third))!=-1) {
+                        candidate_card.add(mycard.get(third));
+                    }
+                    if(checker.check_if_decideable(candidate_card,card_field)){
+                        return candidate_card;
+                    }
+                    for(fourth=fourth+third-3;fourth<mycard.size();fourth++){
+                        if(candidate_card.indexOf(mycard.get(fourth))!=-1) {
+                            candidate_card.add(mycard.get(fourth));
+                        }
+                        if(checker.check_if_decideable(candidate_card,card_field)){
+                            return candidate_card;
+                        }
+                        if(candidate_card.indexOf(mycard.get(fourth))!=-1) {
+                            candidate_card.remove(mycard.get(fourth));
+                        }
+                    }
+                    if(candidate_card.indexOf(mycard.get(third))!=-1) {
+                        candidate_card.remove(mycard.get(third));
+                    }
+                }
+                if(candidate_card.indexOf(mycard.get(second))!=-1) {
+                    candidate_card.remove(mycard.get(second));
+                }
+            }
+            if(candidate_card.indexOf(mycard.get(first))!=-1) {
+                candidate_card.remove(mycard.get(first));
+            }
         }
+//        for(candidate=0;candidate<mycard.size();candidate++){
+//            candidate_card.clear();
+//            candidate_card.add(mycard.get(candidate));
+//            if(checker.check_if_decideable(candidate_card,card_field)){
+//                return candidate_card;
+//            }
+//        }
         return empty_card;
     }
 
