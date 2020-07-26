860f8ffeb6cd9c796cbdeff22339e9433f971b0f
msatoh <msatoh@hotmail.co.jp>
2020-07-26 15:58:30 +0900

    ステータス画面実装中

--- app/src/main/java/com/example/suijaku/StatusActivity.java
+++ app/src/main/java/com/example/suijaku/StatusActivity.java
@@ -1,10 +1,25 @@
 package com.example.suijaku;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class StatusActivity extends View {
+    Paint mpaint = new Paint();
 
-public class StatusActivity extends AppCompatActivity {
+    public StatusActivity(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
 
-
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int mounts;
+        canvas.drawText("Hello world!", 50, 50, mpaint);
+        for (mounts = 0; mounts < 23; mounts++) {
+            canvas.drawCircle(150, 150 + 60 * mounts, 25, mpaint);
+        }
+    }
 }
